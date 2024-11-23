ca8c4af6bdf5e4432d2b08baabe1d094f49413f5
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2024-11-23 12:39:14 +0100

    adding datbase connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{ console.log('database working well')})
+    .catch((err)=>{console.log(err);});
+    
 
 const server = express();
 
